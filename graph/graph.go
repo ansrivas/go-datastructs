225134eed7db692142c574217078ccd774a2cb3c
Ankur Srivastava <ankur.srivastava@recogizer.com>
2017-03-14 16:17:31 +0100

    vertices test

--- graph/graph.go
+++ graph/graph.go
@@ -20,12 +20,30 @@ func (g *Graph) Contains(id int) bool {
 	return ok
 }
 
-//AddNode adds a new node to a graph
-func (g *Graph) AddNode(id int) error {
+//AddVertex adds a new node to a graph
+func (g *Graph) AddVertex(id int) error {
 	ok := g.Contains(id)
 	if ok {
 		return errors.New("Node already exists")
 	}
 	(*g)[id] = newNode(id)
 	return nil
+}
+
+//ListVertices gives a list of all the vertices in this graph
+func (g *Graph) ListVertices() []*GNode {
+	nodes := make([]*GNode, 0)
+	for k := range *g {
+		nodes = append(nodes, (*g)[k])
+	}
+	return nodes
+}
+
+//ListVerticesID gives a list of all the verticx-id (unique identifier of a node) in this graph
+func (g *Graph) ListVerticesID() []int {
+	nodes := make([]int, 0)
+	for k := range *g {
+		nodes = append(nodes, (*g)[k].id)
+	}
+	return nodes
 }
--- graph/graph_test.go
+++ graph/graph_test.go
@@ -1,6 +1,7 @@
 package graph
 
 import (
+	"sort"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,9 +10,9 @@ import (
 func Test_GraphNodeCreation(t *testing.T) {
 	assert := assert.New(t)
 	graph := NewGraph()
-	graph.AddNode(1)
-	graph.AddNode(2)
-	graph.AddNode(3)
+	graph.AddVertex(1)
+	graph.AddVertex(2)
+	graph.AddVertex(3)
 
 	assert.True(graph.Contains(1), "graph should contain node.id 1")
 	assert.True(graph.Contains(2), "graph should contain node.id 2")
@@ -19,7 +20,30 @@ func Test_GraphNodeCreation(t *testing.T) {
 	assert.False(graph.Contains(5), "graph should NOT contain node.id 5")
 	assert.False(graph.Contains(6), "graph should NOT contain node.id 6")
 
-	err := graph.AddNode(1)
+	err := graph.AddVertex(1)
 	assert.NotNil(err, "Should not allow adding the same node again")
+}
 
+func Test_Vertices(t *testing.T) {
+	assert := assert.New(t)
+
+	graph := NewGraph()
+	graph.AddVertex(1)
+	graph.AddVertex(2)
+	graph.AddVertex(3)
+
+	vertices := graph.ListVerticesID()
+	sort.Ints(vertices)
+	actual := []int{1, 2, 3}
+	assert.EqualValues(vertices, actual, "ListVerticesID should get 3 node-ids")
+
+	vertexList := graph.ListVertices()
+
+	expected := make([]int, 0)
+	for _, node := range vertexList {
+		expected = append(expected, node.id)
+	}
+	sort.Ints(expected)
+	actual = []int{1, 2, 3}
+	assert.EqualValues(expected, actual, "ListVertices should get 3 node-ids")
 }
