11888325942da30f23d362776ff60645dd4599b0
Ankur Srivastava <ankur.srivastava@recogizer.com>
2017-03-24 00:28:59 +0100

    trie implementation

---
+++ trie/trie.go
@@ -0,0 +1,69 @@
+package trie
+
+import "errors"
+
+type TrieNode struct {
+	children  map[rune]*TrieNode
+	isEndNode bool
+}
+
+type Trie struct {
+	root *TrieNode
+}
+
+var (
+	EmptyStringError = errors.New("Empty string inserted.")
+	EmptyRootError   = errors.New("Trie has not been initialized.")
+)
+
+//NewNode creates a new TrieNode
+func NewNode(isEndNode bool) *TrieNode {
+	node := TrieNode{children: make(map[rune]*TrieNode), isEndNode: isEndNode}
+	return &node
+}
+
+//NewTrie creates a new trie.
+func NewTrie() *Trie {
+	root := Trie{root: NewNode(false)}
+	return &root
+}
+
+//Insert a string into trie
+func (trie *Trie) Insert(input string) error {
+	if input == "" {
+		return EmptyStringError
+	}
+	current := trie.root
+	var newNode *TrieNode
+	for _, char := range input {
+		node, ok := current.children[char]
+		if ok {
+			current = node
+		} else {
+			newNode = NewNode(false)
+			current.children[char] = newNode
+			current = newNode
+		}
+	}
+	// Mark last node as true
+	current.isEndNode = true
+	return nil
+}
+
+//StartsWith checks if there is any input in trie which starts with given start string.
+func (trie *Trie) Search(input string) (bool, error) {
+	if input == "" {
+		return false, EmptyStringError
+	}
+
+	current := trie.root
+
+	for _, char := range input {
+		next, ok := current.children[char]
+		if !ok {
+			return false, nil
+		}
+		current = next
+	}
+	return current.isEndNode, nil
+}
---
+++ trie/trie_test.go
@@ -0,0 +1,28 @@
+package trie
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_TrieInsert(t *testing.T) {
+	assert := assert.New(t)
+
+	trie := NewTrie()
+	err := trie.Insert("")
+	assert.NotNil(err, "Error should not be nil")
+	assert.Equal(err.Error(), "Empty string inserted.", "Emtpy string added")
+
+	trie.Insert("ankur")
+	trie.Insert("ana")
+
+	_, err = trie.Search("")
+	assert.NotNil(err, "Error should not be nil")
+	assert.Equal(err.Error(), "Empty string inserted.", "Emtpy string added")
+
+	found, _ := trie.Search("ankur")
+	assert.True(found, "Ankur was in trie")
+	found, _ = trie.Search("viv")
+	assert.False(found, "viv should not be in trie")
+}
