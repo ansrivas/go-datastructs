c5dd1c451ad8cbc8c0951249aafa518b8a237231
Ankur Srivastava <ankur.srivastava@recogizer.com>
2017-04-01 23:02:49 +0200

    dirty solution, will improve later, linear time + O(windowlen) space

---
+++ commonProbs/distinctElementWindowed.go
@@ -0,0 +1,70 @@
+package commonProbs
+
+import "errors"
+
+// You are given an array of size n.
+// Now print the number of distinct elements in the array for every frame of size k.
+
+type DistinctWindowedArr struct {
+	windowLen int
+	input     []int
+	dict      map[int]int
+}
+
+//NewDistinctWindowed returns a given window len + inputy values
+func NewDistinctWindowed(windowLen int, input []int) *DistinctWindowedArr {
+	return &DistinctWindowedArr{
+		windowLen: windowLen,
+		input:     input,
+		dict:      make(map[int]int),
+	}
+}
+
+func (ds *DistinctWindowedArr) GetDistElemInWindow() ([]int, error) {
+	length := len(ds.input)
+	output := make([]int, 0)
+	if length < ds.windowLen {
+		return nil, errors.New("Window is larger than actual array")
+	}
+
+	startIdx := 0
+	//For the first initialization
+	for i := startIdx; i < ds.windowLen+startIdx; i++ {
+		key := ds.input[i]
+		node, ok := (*ds).dict[key]
+		if ok {
+			(*ds).dict[key] = node + 1
+		} else {
+			(*ds).dict[key] = 1
+		}
+	}
+
+	for {
+		output = append(output, len(ds.dict))
+
+		//Case if the previous value occurs more than one time.
+		key := ds.input[startIdx]
+		node, _ := (*ds).dict[key]
+		if node == 1 {
+			delete((*ds).dict, key)
+		} else {
+			(*ds).dict[key] = node - 1
+		}
+
+		if startIdx+ds.windowLen < length {
+			//case where next value is already present or needs to be inserted.
+			nextKey := ds.input[startIdx+ds.windowLen]
+			node, ok := (*ds).dict[nextKey]
+			if ok {
+				(*ds).dict[nextKey] = node + 1
+			} else {
+				(*ds).dict[nextKey] = 1
+			}
+			startIdx++
+		} else {
+			break
+		}
+	}
+
+	return output, nil
+}
---
+++ commonProbs/distinctElementWindowed_test.go
@@ -0,0 +1,25 @@
+package commonProbs
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_NewDistinctWindowed(t *testing.T) {
+	assert := assert.New(t)
+
+	input := []int{2, 1, 1, 3, 5, 2, 6, 6, 7}
+	expected := []int{2, 2, 3, 3, 3, 2, 2}
+	windowLen := 3
+
+	ds := NewDistinctWindowed(windowLen, input)
+	nodes, _ := ds.GetDistElemInWindow()
+	assert.Equal(expected, nodes, "Count of distinct elements should be same.")
+
+	input = []int{2, 1}
+	ds = NewDistinctWindowed(windowLen, input)
+	_, err := ds.GetDistElemInWindow()
+	assert.NotNil(err, "Passing window length larger than input length should return error")
+	assert.True(true, "true")
+}
